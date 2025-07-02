ba1341a9ab28d68707e463d66b67fe269f7567ce
Cédric Couliou <cedric.couliou@gmail.com>
2025-07-02 23:32:55 +0200

    + comment

--- example.sql
+++ example.sql
@@ -1,5 +1,15 @@
 /* Test file */
+select lower(table_name), count(*) 
+    from user_tables t
+    join user_tab_cols c on c.table_name = t.table_name
+    where table_name = 'DUAL'
+    group by lower(table_name);
+
 begin
-    proc_with_sql_param(/* SQL */ 'select dummy
-        from dual');
+    proc_with_sql_param(/* SQL */ 'select lower(table_name), count(*) 
+            from user_tables t
+            join user_tab_cols c on c.table_name = t.table_name
+            where table_name = :table_name
+            group by lower(table_name)',
+        'table_name' => 'DUAL');
 end;
